5b9a33de0fb251f34d2baee341b845f4407c9fa6
MariaMlk <49075059+MariaMlk@users.noreply.github.com>
2019-05-04 19:42:35 +0500

    Add files

---
+++ DataCollectingApp/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Xml.Serialization;
+
+namespace DataCollectingApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ViewModel Model { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Model = new ViewModel();
+            DataContext = Model;
+            Genders.ItemsSource = new[] { "Мужской", "Женский" };
+        }
+
+        private void SaveClick(object sender, RoutedEventArgs e)
+        {
+            var saveDialog = new SaveFileDialog();
+            if (saveDialog.ShowDialog() == true)
+            {
+                var filePath = saveDialog.FileName;
+                var xmlSerializer = new XmlSerializer(typeof(ViewModel));
+
+                using (var writer = new StreamWriter(filePath))
+                {
+                    xmlSerializer.Serialize(writer, Model);
+                }                
+            }
+        }
+
+        private void LoadClick(object sender, RoutedEventArgs e)
+        {
+            var openDialog = new OpenFileDialog();
+            if(openDialog.ShowDialog() == true)
+            {
+                var filePath = openDialog.FileName;
+
+                var xmlSerializer = new XmlSerializer(typeof(ViewModel));
+
+                using (var fs = new FileStream(filePath, FileMode.Open))
+                {
+                    Model = (ViewModel)xmlSerializer.Deserialize(fs);
+                    DataContext = Model;
+                }
+            }
+        }
+
+        private void TextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            var regex = new Regex("[^0-9\\+\\(\\)-]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
---
+++ DataCollectingApp/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataCollectingApp
+{
+    public class ViewModel
+    {
+        public string FullName { get; set; }
+        public int? Age { get; set; } = null;
+        public string PhoneNumber { get; set; }
+        public string Gender { get; set; }
+        public double? Weight { get; set; } = null;
+        public double? Height { get; set; } = null;
+        public string Recommendations { get; set; }
+        public DateTime? VisitDate { get; set; } = null;
+        public List<Exercise> Exercises { get; set; } = new List<Exercise>();
+    }
+
+    public class Exercise
+    {
+        public string Name { get; set; }
+        public int? RepeatsNumber { get; set; } = null;
+        public int? Minutes { get; set; } = null;
+        public string Comment { get; set; }
+    }
+}
